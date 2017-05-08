bc3afabac604ebfd7bc78b3c29acc78ce0138cec
dankato <apearmor@gmail.com>
2017-05-08 11:41:08 -0700

    updated file, throw error

--- awesomefile.js
+++ awesomefile.js
@@ -1,7 +1,19 @@
+
 function whoAmI (name, age) {
-	const yearOf = (2016 - age);
-console.log(`My name is ${name} and I'm ${age} years old`);
-console.log("I was born in " + yearOf);
+	const yearOf = function(age) {
+		if (age <= 0) {
+			throw new Error('Age cannot be negative.');
+		} else {
+			return 2016 - age;
+		}
+	}
+	console.log(`My name is ${name} and I'm ${age} years old`);
+	console.log("I was born in " + yearOf(age));
 }
 
-whoAmI ("Nick", 29);
+try {
+	whoAmI ("Nick", -29);
+}
+catch (e) {
+	console.error(e);
+}
