4f330743432f5e18a197d91d0992d78c10e669ff
tckastanek <tckastanek@gmail.com>
2017-05-08 17:30:30 -0400

    end of the day

--- awesomefile5.js
+++ awesomefile5.js
@@ -1,5 +1,3 @@
-
-
 function decode(code) {
   var firstLetter = code.charAt(0);
   if (firstLetter === 'a') {
@@ -14,11 +12,48 @@ function decode(code) {
     return ' ';
   }
 }
-console.log(decode('craft'));
-console.log(decode('block'));
-console.log(decode('argon'));
-console.log(decode('meter'));
-console.log(decode('bells'));
-console.log(decode('brown'));
-console.log(decode('croon'));
-console.log(decode('droop'));
+
+// function decode_sentence(sentence) {
+//   const words = sentence.split(' ');
+//   return words.map(function(word) {
+//     return oneWord(word);
+//   }).join('');
+// }
+//
+function decode_sentence(sentence) {
+  const words = sentence.split(' ');
+  let result = [];
+  for (i = 0; i < words.length; i++) {
+    result.push(decode(words[i]));
+  }
+  return result.join('');
+}
+
+// console.log(decode_sentence('craft block argon meter bells brown croon droop'));
+
+function stupid(n) {
+  let result1 = "", result2 = "";
+  for (let i = 0; i < n; i++)
+    for (let j = 0; j < n; j++)
+      for (let k = 0; k < n; k++) {
+        result2 = result1 + "q";
+        result1 = result2 + "w";
+      }
+  return result1;
+}
+
+// for (i = 0; i
+for (let n = 10; n <= 100; n += 10)
+  console.log(n, stupid(n).length)
+
+
+
+
+// console.log(decode('craft'));
+// console.log(decode('block'));
+// console.log(decode('argon'));
+// console.log(decode('meter'));
+// console.log(decode('bells'));
+// console.log(decode('brown'));
+// console.log(decode('croon'));
+// console.log(decode('droop'));
--- awesomefile7.js
+++ awesomefile7.js
@@ -1,22 +1,23 @@
 function RockPaperScissors(userChoice){
     const computer = 1 + Math.floor((Math.random()*3));
     const winning = userChoice - computer;
-    // const rpc = [
-    //   {roll: 'Scissors', val: 1}
-    //   {roll: 'Paper', val: 2}
-    //   {roll: 'Rock', val: 3}
-    // ];
+    const rpc = [
+      {roll: 'Scissors', val: 1},
+      {roll: 'Paper', val: 2},
+      {roll: 'Rock', val: 3},
+    ];
+    console.log(`User:${rpc[userChoice -1].roll}, Computer:${rpc[computer -1].roll}`)
     if (winning === 0) {
-      console.log(`User:${userChoice}, Computer:${computer}`);
+      // console.log(`User:${rpc.roll}, Computer:${computer}`);
       console.log('You tied!');
     } else if (winning === -1 || winning === 2) {
-      console.log(`User:${userChoice}, Computer:${computer}`);
+      // console.log(`User:${rpc.roll}, Computer:${computer}`);
       console.log('You win!');
     } else {
-      console.log(`User:${userChoice}, Computer:${computer}`);
+      // console.log(`User:${rpc.roll}, Computer:${computer}`);
       console.log('You lose!');
     }
 
 }
 
-RockPaperScissors(3);
+RockPaperScissors(4);
