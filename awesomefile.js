a231310a8571a26097e12a6259e65099df2eb11d
nfjones87 <nfjones87@gmail.com>
2017-05-08 11:06:02 -0700

    fourth commit

--- awesomefile.js
+++ awesomefile.js
@@ -1,5 +1,7 @@
 function whoAmI (name, age) {
+	const yearOf = (2016 - age);
 console.log(`My name is ${name} and I'm ${age} years old`);
+console.log("I was born in " + yearOf);
 }
 
 whoAmI ("Nick", 29);
